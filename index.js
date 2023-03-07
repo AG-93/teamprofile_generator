7594b19251dcffb37c1fd9e37ccc2ef1728b4558
AG-93 <apechhya.gurung@gmail.com>
2023-03-07 22:06:10 +0000

    updated index.js function buildTeam()

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const teamMembers = []
 const appMenu = () => {
 
     function buildTeam(){
+        if(!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), 'utf-8');
 
     }
 
@@ -47,7 +51,7 @@ const appMenu = () => {
             const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internGithub)
             teamMembers.push(intern);
             idList.push(answers.internId);
-            console.log(intern);
+            //console.log(intern);
             createTeam()
         })
 
@@ -80,7 +84,7 @@ const appMenu = () => {
             const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
             teamMembers.push(engineer);
             idList.push(answers.engineerId);
-            console.log(engineer);
+            //console.log(engineer);
             createTeam()
         })
 
@@ -144,7 +148,7 @@ const appMenu = () => {
     
         ]).then(answers => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
-            console.log(manager);
+            //console.log(manager);
             teamMembers.push(manager);
             idList.push(answers.managerId);
             createTeam();
